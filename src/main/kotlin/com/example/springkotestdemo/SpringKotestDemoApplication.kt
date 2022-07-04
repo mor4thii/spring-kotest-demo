8c6ecc48d70530c847127170001d129861bfe68a
Morathii <morathii@icloud.com>
2022-07-04 10:36:21 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/springkotestdemo/SpringKotestDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkotestdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotestDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotestDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springkotestdemo/SpringKotestDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springkotestdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotestDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
